4e4c9c933ca823a8642d75c017a4e85f04884d77
ihateupants <dontuhatepants.gh@gmail.com>
2016-12-01 16:49:44 +1100

    My first commit

--- SwiftTrainingApp/ViewController.swift
+++ SwiftTrainingApp/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Linked label to code. Able to call label further down and change value
+    @IBOutlet weak var LabelOne: UILabel!
+    
+    var tapCount = 0
+    
+    // action to execute on button press
+    @IBAction func PushMeAction(_ sender: Any) {
+    
+        //LabelOne.text = "Swift testing" // label value changed (after action)
+        //print("Button Pressed") // 'Print' purely for debugging
+    
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount >= 10 {
+            LabelOne.text = "Tap King!"
+        }
+    }
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        view.backgroundColor = UIColor.red // set background colour of view
     }
 
     override func didReceiveMemoryWarning() {
